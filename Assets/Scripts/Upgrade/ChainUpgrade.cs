fa1322a67f63c1d64042ae47c6bdf6623af5c493
Baiyan Liu <baiyanliu@protonmail.com>
2021-06-25 08:42:45 -0400

    Adding tower upgrade that adds a chain attack effect.

---
+++ Assets/Scripts/Upgrade/ChainUpgrade.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Assets.Scripts.Effect;
+using JetBrains.Annotations;
+
+namespace Assets.Scripts.Upgrade
+{
+    [UsedImplicitly]
+    public sealed class ChainUpgrade : EffectUpgrade<ChainEffect>
+    {
+        public float[] Range;
+
+        public override string Name => "Chain Upgrade";
+        protected override string AmountName => "Damage";
+
+        protected override void OnLevelUp()
+        {
+            base.OnLevelUp();
+            Effect.Amount.Value = Amount[Level];
+            Effect.Range.Value = Range[Level];
+        }
+
+        public override List<string> GetAmountDisplayText()
+        {
+            var amountDisplayText = base.GetAmountDisplayText();
+            amountDisplayText.Add(FormatDisplayText("Range", Range, false));
+            return amountDisplayText;
+        }
+    }
+}

802ec3d4583b735df950b7903c9dc4433d598887
Baiyan Liu <baiyanliu@protonmail.com>
2021-06-26 10:41:51 -0400

    Adding a few more upgrade types.

---
+++ Assets/Scripts/Upgrade/SlowUpgrade.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Assets.Scripts.Effect;
+using JetBrains.Annotations;
+
+namespace Assets.Scripts.Upgrade
+{
+    [UsedImplicitly]
+    public sealed class SlowUpgrade : EffectUpgrade<SlowEffect>
+    {
+        public float[] Duration;
+
+        public override string Name => "Slow Upgrade";
+
+        protected override void OnLevelUp()
+        {
+            base.OnLevelUp();
+            Effect.Amount.Value = Amount[Level];
+            Effect.Duration.Value = Duration[Level];
+        }
+
+        public override List<string> GetAmountDisplayText()
+        {
+            var amountDisplayText = base.GetAmountDisplayText();
+            amountDisplayText.Insert(0, FormatDisplayText("Duration", Duration, false));
+            return amountDisplayText;
+        }
+    }
+}
--- Assets/Scripts/Upgrade/SplashUpgrade.cs
+++ Assets/Scripts/Upgrade/SplashUpgrade.cs
@@ -4,20 +4,15 @@ using JetBrains.Annotations;
 namespace Assets.Scripts.Upgrade
 {
     [UsedImplicitly]
-    public sealed class ExistingSplashUpgrade : ExistingEffectUpgrade<SplashEffect>
+    public sealed class SplashUpgrade : EffectUpgrade<SplashEffect>
     {
         public override string Name => "Splash Upgrade";
         protected override string AmountName => "Range";
 
         protected override void OnLevelUp()
         {
-            var delta = Amount[Level];
-            if (Level > 0)
-            {
-                delta -= Amount[Level - 1];
-            }
-            Effect.Amount.Base += delta;
-            Effect.Amount.Value += delta;
+            base.OnLevelUp();
+            Effect.Amount.Value = Amount[Level];
         }
     }
 }
